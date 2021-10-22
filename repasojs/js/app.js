01f53bfab569549dfce894a426c66ac4e33f5e2c
Jean Vega <jeanv046@gmail.com>
2021-10-22 13:07:20 -0500

    Promises

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -248,3 +248,21 @@ let total = persona.reduce((edadTotal, persona)=>{
     return edadTotal + persona.edad;
 }, 0);
 console.log(total / persona.length) */
+
+//Promises
+/* const aplicarDescuento = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        let descuento = true;
+        if (descuento) {
+            resolve('Descuento aplicado!');
+        }else {
+            reject('No se pudo aplicar descuento')
+        }
+
+    }, 3000);
+});
+aplicarDescuento.then(resultado => {
+    console.log(resultado);
+}).catch(error => {
+    console.log(error);
+}) */

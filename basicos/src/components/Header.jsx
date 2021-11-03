2a20dde13bdf06ce51263e32b563b7bc52a38084
Jean Vega <jeanv046@gmail.com>
2021-11-03 11:21:44 -0500

    componente Header

--- basicos/src/components/Header.jsx
+++ basicos/src/components/Header.jsx
@@ -3,8 +3,15 @@ import React from "react";
 
 function Header() {
     
+    const edad = 17;
+    let mensaje;
+    if (edad >= 18) {
+        mensaje = 'Eres mayor de edad';
+    }else{
+        mensaje = 'Eres menor de edad';
+    }
     return(
-        <h1>Desde el header</h1>
+        <h1>Desde el header {edad} , {mensaje}</h1>
         
     )
 }

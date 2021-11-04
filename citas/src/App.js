1f0c7fec3544c6a911e6d8145eca96be94d2a660
Jean Vega <jeanv046@gmail.com>
2021-11-04 14:34:25 -0500

    Componente de formulario

--- citas/src/App.js
+++ citas/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
-
+import React, { Fragment } from "react";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
-    <h1>Administrados de Pacientes</h1>
+    <Fragment>
+      <h1>Administrados de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">
+            2
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 

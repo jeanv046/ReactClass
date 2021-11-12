8aafa20be18c7d27e2a0baff51571190870ddfbb
Jean Vega <jeanv046@gmail.com>
2021-11-12 11:50:21 -0500

    Input presupuesto

--- presupuesto/src/App.js
+++ presupuesto/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Pregunta from './components/Pregunta';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactd
-        </a>
+    <div className="container">
+      <header>
+        <h1>Gasto Semanal</h1>
+
+        <div className="contenido-principal contenido">
+          <Pregunta />
+        </div>
       </header>
     </div>
   );

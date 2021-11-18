23c3e478d62a8b915a280ebe4143dac6a7b3e448
Jean Vega <jeanv046@gmail.com>
2021-11-18 15:26:00 -0500

    validando gastos

--- presupuesto/src/components/Formulario.js
+++ presupuesto/src/components/Formulario.js
@@ -1,18 +1,24 @@
 import React, {useState} from 'react'
+import Error from './Error'
 
 const Formulario = () => {
 
     const [nombre, guardarNombre] = useState('');
     const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
 
     //cuando el usuario agrega un gasto
     const agregarGasto = e => {
         e.preventDefault();
 
         //Validar
-
+        if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === '') {
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
         //construir el gasto al componente principal
-
+        
         //Resetear el form
     }
 
@@ -21,6 +27,9 @@ const Formulario = () => {
             onSubmit={agregarGasto}
         >
             <h2>Agrega tus gastos aqui</h2>
+
+            {error ? <Error mensaje="Ambos campos son obligatorios o Presupuesto Incorrecto"/> : null}
+
             <div className="campo">
                 <label>Nombre del gasto</label>
                 <input 

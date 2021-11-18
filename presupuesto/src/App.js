0ed1fed7b1e5e0552a6ad943b033c1ae70672548
Jean Vega <jeanv046@gmail.com>
2021-11-18 17:07:35 -0500

    componente presupuesto y restante

--- presupuesto/src/App.js
+++ presupuesto/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Pregunta from './components/Pregunta';
 import Formulario from './components/Formulario';
 import Listado from './components/Listado';
+import ControlPresupuesto from './components/ControlPresupuesto';
 function App() {
 
   //Definir el state
@@ -38,6 +39,10 @@ function App() {
             <div className="one-half column">
                 <Listado
                   gastos={gastos}
+                />
+                <ControlPresupuesto
+                  presupuesto={presupuesto}
+                  restante={restante}
                 />
             </div>
           </div>
